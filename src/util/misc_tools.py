63fbc846f418e3ce002dd6325e3ed7e0827e4655
SKCrawford <s.kyle.crawford@gmail.com>
2020-08-24 18:27:28 -0700

    Add lemmas to the print token table function.

--- src/util/misc_tools.py
+++ src/util/misc_tools.py
@@ -3,11 +3,12 @@ from spacy import explain
 from src.nlp import nlp
 
 
-def print_token_table(sentence, pos=True, tag=True, dependency=True):
+def print_token_table(sentence, pos=True, tag=True, dependency=True, lemma=True):
     """Print the linguistics features of each word in a sentence.
     If pos is True, then print the part-of-speech (POS). Defaults to True.
     If tag is True, then print the tag. Defaults to True.
     If dependency is True, then print the dependencices. Defaults to True.
+    If lemma is True, then print the lemma. Defaults to False.
 
     Given a string, return None.
     """
@@ -23,6 +24,8 @@ def print_token_table(sentence, pos=True, tag=True, dependency=True):
     if dependency:
         headers.append("Dep.")
         headers.append("Dep. Definition")
+    if lemma:
+        headers.append("Lemma.")
 
     # Create the table data
     tagged_words = nlp(sentence)
@@ -39,6 +42,8 @@ def print_token_table(sentence, pos=True, tag=True, dependency=True):
         if dependency:
             entry.append(word.dep_)
             entry.append(explain(word.dep_))
+        if lemma:
+            entry.append(word.lemma_)
         data.append(entry)
 
     # Print the table
