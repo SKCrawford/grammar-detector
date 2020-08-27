a3d9df97ea1a57380fbfd3e70fdb6f6ca07a636b
SKCrawford <s.kyle.crawford@gmail.com>
2020-08-26 17:02:33 -0700

    Update the tests to use the generic Builder.

--- tests/util/test_builder.py
+++ tests/util/test_builder.py
@@ -1,38 +1,34 @@
 import unittest
-from src.features.verb_set.builder import VerbFeatureSetBuilder
+from src.util.builder import Builder
 from src.features.verb_set.model import VerbFeatureSet
 
 
-class TestVerbFeatureSetBuilder(unittest.TestCase):
+class TestBuilder(unittest.TestCase):
     builder = None
 
     def setUp(self):
-        self.builder = VerbFeatureSetBuilder()
+        self.builder = Builder(VerbFeatureSet)
 
     def test_is_defined(self):
-        self.assertTrue(VerbFeatureSetBuilder)
+        self.assertTrue(Builder)
 
     def test_is_constructable(self):
-        self.assertTrue(VerbFeatureSetBuilder())
+        self.assertTrue(Builder(VerbFeatureSet))
 
     def test_can_spawn_product_instance(self):
-        self.assertIsInstance(self.builder, VerbFeatureSetBuilder)
+        self.assertIsInstance(self.builder, Builder)
         self.assertTrue(self.builder)
 
-    def test_has_no_product_instance_until_spawn(self):
+    def test_has_no_product_instance_until_spawned(self):
         self.assertFalse(self.builder._instance)
         self.builder.spawn()
         self.assertTrue(self.builder._instance)
 
     def test_can_build_complete_product_instance(self):
-        instance = VerbFeatureSetBuilder()              \
-            .spawn()                                    \
+        instance = Builder(VerbFeatureSet)              \
             .set_attr('verb', "have been running")      \
             .set_attr('tense', "present")               \
             .set_attr('aspect', "perfect continuous")   \
-            .set_attr('voice', "active")                \
-            .set_attr('person', "1st")                  \
-            .set_attr('lemmas', "have be run")          \
             .build()
         self.assertIsInstance(instance, VerbFeatureSet)
         self.assertTrue(instance)
