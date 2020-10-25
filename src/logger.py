d6546e019f51a04bef9576998143538ee017296f
SKCrawford <s.kyle.crawford@gmail.com>
2020-10-25 15:03:07 -0700

    Add a last.log to only keep track of the last run.

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,10 @@
 import logging
 
 
-logging.basicConfig(
-    filename="info.log",
-    format="[%(asctime)s][%(module)s:%(funcName)s:%(lineno)d][%(levelname)s] %(message)s",
-    level=logging.DEBUG,
-)
+FORMAT ="[%(asctime)s][%(module)s:%(funcName)s:%(lineno)d][%(levelname)s] %(message)s" 
+LEVEL = logging.DEBUG
+
+last_filehandler = logging.FileHandler("last.log", mode="w")
+debug_filehandler = logging.FileHandler("debug.log")
+
+logging.basicConfig(format=FORMAT, level=LEVEL, handlers=[last_filehandler, debug_filehandler])
