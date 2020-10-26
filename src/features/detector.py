6398c03335ad33a88293bd5a47fcc8e475598dd3
SKCrawford <s.kyle.crawford@gmail.com>
2020-10-26 14:26:13 -0700

    Refine the similarity detector. Use it to determine the similarity scores between the verbs and the nouns.

--- src/features/detector.py
+++ src/features/detector.py
@@ -1,5 +1,6 @@
 import logging
 from .noun_set import detect_noun_features
+from .similarity import detect_similarities_between_verbs_and_nouns
 from .verb_set import detect_verb_features
 
 
@@ -8,10 +9,10 @@ logger = logging.getLogger(__name__)
 
 def detect_features(sentence):
     """The main entry point for the feature detector."""
-
     logger.debug("Started detecting")
     nouns = detect_noun_features(sentence)
     verbs = detect_verb_features(sentence)
+    (verbs, nouns) = detect_similarities_between_verbs_and_nouns(verbs, nouns)
 
     feature_set = {}
     feature_set["sentence"] = sentence
---
+++ src/features/similarity.py
@@ -0,0 +1,46 @@
+import logging
+from src.util.spacy import get_span, make_doc
+
+
+logger = logging.getLogger(__name__)
+
+
+def detect_similarity(base_word, target_words):
+    """Determine the similarity scores between a word and a list of words.
+    Produces a list of tuples of shape (similarity_score, target_span, base_span).
+
+    Given 0) a string, Span, or Doc and 1) a list of strings, Spans, or Docs,
+    return a list of tuples of (float, Span, Span).
+    """
+    logger.debug("Started detecting")
+    logger.debug(f"Comparing base word `{base_word}` to target words `{target_words}`")
+    similarities = []
+    base_span = get_span(base_word)
+    for target_word in target_words:
+        target_span = get_span(target_word)
+        similarity = base_span.similarity(target_span)
+
+        logger.debug(f"Similarity between `{base_word}` and `{target_word}` is `{similarity}`")
+        entry = (similarity, target_span, base_span)
+        similarities.append(entry)
+    return similarities
+
+
+def detect_similarities_between_verbs_and_nouns(verbs, nouns):
+    """For each verb and noun, add an attribute called `similarities`,
+    which holds the similarity scores relevant to each verb and noun.
+
+    Immutable. Given a list and a list, return a tuple of (list, list).
+    """
+    # Immutably add the attribute "similarity" to each dict in the list
+    verbs = [dict(verb, **{ "similarities": [] }) for verb in list.copy(verbs)]
+    nouns = [dict(noun, **{ "similarities": [] }) for noun in list.copy(nouns)]
+
+    for verb in verbs:
+        entries = detect_similarity(verb["span"], [noun["span"] for noun in nouns])
+        noun_i = 0 # The order of nouns matches the order of similarity entries
+        for entry in entries:
+            verb["similarities"].append(entry)
+            nouns[noun_i]["similarities"].append(entry)
+            noun_i += 1
+    return (verbs, nouns)
