7671c1e3e7f50296e90f55415d46130238725b6b
SKCrawford <s.kyle.crawford@gmail.com>
2020-10-26 15:42:26 -0700

    Add a simple timer for measuring the run time for each sentence and total.

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,7 @@
 import logging
 import pprint
 import sys
+import time
 import src.logger
 from src.util.misc_tools import to_token_table
 from .features import detect_features
@@ -10,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 def main():
+    start_time = time.time()
     sentences = []
     if len(sys.argv) > 0:
         sentences = sys.argv[1:]
@@ -18,6 +20,7 @@ def main():
 
     count = 0
     for sentence in sentences:
+        sentence_start_time = time.time()
         logger.info(f"Sentences ({len(sentences)}): `{sentences}`")
         print(f"####### START SENTENCE {count} #######")
         logger.info(f"Sentence {count}: `{sentence}`")
@@ -31,8 +34,10 @@ def main():
         logger.info("Detected features:")
         logger.info(pprint.pformat(feature_set))
         pprint.pprint(feature_set)
+        print(f"Sentence run time: {time.time() - sentence_start_time:.2f}s")
         print(f"####### END SENTENCE {count} #######")
         count += 1
+    print(f"Total run time: {time.time() - start_time:.2f}s")
 
 
 if __name__ == "__main__":
