7fc8b2b02668952c71c73af9be59ea9a22654aaf
SKCrawford <s.kyle.crawford@gmail.com>
2020-10-26 15:11:52 -0700

    Export the similarity detector from the features module.

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,3 +1,4 @@
 from .detector import detect_features
 from .noun_set import detect_noun_determiner, detect_noun_features, detect_noun_person, detect_nouns
+from .similarity import detect_similarity
 from .verb_set import detect_verb_features, detect_verb_tense_aspect, detect_verb_voice, detect_verbs
