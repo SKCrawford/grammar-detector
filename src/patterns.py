694f8d3833e6a6663afed0c9b307f781087bf87f
SKCrawford <s.kyle.crawford@gmail.com>
2021-09-23 16:23:21 -0700

    Bugfix: now correctly throws exceptions when problems loading patterns arise.

--- src/patterns.py
+++ src/patterns.py
@@ -11,12 +11,13 @@ def load_patternset(patternset_filename):
     """Given a filepath ending with .json, """
     patternset_path = join(PATTERNS_DIR, f"{patternset_filename}.json")
     logger.debug(f"Loading {patternset_path}")
-    with open(patternset_path, "r") as f:
-        try:
+
+    try:
+        with open(patternset_path, "r") as f:
             return load(f)
-        except FileNotFoundError as e:
-            logger.error(f"Couldn't find {patternset_path}")
-            raise e
-        except Exception as e:
-            logger.error(f"Unknown error occurred while loading {patternset_path}")
-            raise e
+    except FileNotFoundError as e:
+        logger.error(f"Couldn't find {patternset_path}")
+        raise e
+    except Exception as e:
+        logger.error(f"Unknown error occurred while loading {patternset_path}")
+        raise e
