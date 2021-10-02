5903265964b65aee10c50e03da9258dd61c42c61
SKCrawford <s.kyle.crawford@gmail.com>
2021-10-02 11:06:44 -0700

    Add support for processing a span's noun chunks instead of its text.

--- src/Matcher.py
+++ src/Matcher.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from spacy.matcher import Matcher as SpacyMatcher
 from . import validators
-from .extractors import get_doc, extract_span_features
+from .extractors import extract_span_features
 from .nlp import nlp
 
 
@@ -13,45 +13,41 @@ class Matcher:
     def __init__(self):
         self._inner_matcher = None
         self._pattern_set = None
-        self._doc = None
 
-    def match_one(self, pattern_set, maybe_tokenized):
+    def match_one(self, pattern_set, doc):
         logger.debug("Preparing to run the external matcher")
-        self._pre_match(pattern_set, maybe_tokenized)
+        self._pre_match(pattern_set)
 
         logger.debug("Running the external matcher")
-        all_matches = self._run_matcher(self._doc)
+        all_matches = self._run_matcher(doc)
         logger.debug(f"Found '{len(all_matches)}' match(es)")
 
         logger.debug("Determining the best match")
         best_match = self._get_best_match(all_matches)
 
         logger.debug(f"Parsing the best match: {best_match}")
-        parsed_match = self._parse_match(best_match, self._doc)
+        parsed_match = self._parse_match(best_match, doc)
 
         logger.debug(f"Parsed one match: {parsed_match}")
         return parsed_match
 
-    def match_many(self, pattern_set, maybe_tokenized):
+    def match_many(self, pattern_set, doc):
         logger.debug("Preparing to run the external matcher")
-        self._pre_match(pattern_set, maybe_tokenized)
+        self._pre_match(pattern_set)
 
         logger.debug("Running the external matcher")
-        all_matches = self._run_matcher(self._doc)
+        all_matches = self._run_matcher(doc)
         logger.debug(f"Found '{len(all_matches)}' match(es)")
 
         logger.debug(f"Parsing many matches: {all_matches}")
-        parsed_matches = [self._parse_match(m, self._doc) for m in all_matches]
+        parsed_matches = [self._parse_match(m, doc) for m in all_matches]
 
         logger.debug(f"Parsed many matches: {parsed_matches}")
         return parsed_matches
 
-    def _pre_match(self, pattern_set, maybe_tokenized):
+    def _pre_match(self, pattern_set):
         logger.debug(f"Creating the internal spaCy matcher")
         self._inner_matcher = self._create_matcher(pattern_set)
-
-        logger.debug(f"Tokenizing `{maybe_tokenized}`")
-        self._doc = get_doc(maybe_tokenized)
 
     def _on_match(self, matcher, doc, i, matches):
         logger.debug(f"MATCH #{i}")
@@ -77,9 +73,9 @@ class Matcher:
             matcher.add(rulename, [tokens], **config)
         return matcher
 
-    def _run_matcher(self, tokenized):
-        logger.debug(f"Running the internal matcher on tokenized '{tokenized}'")
-        return self._inner_matcher(tokenized)
+    def _run_matcher(self, doc):
+        logger.debug(f"Running the internal matcher on doc '{doc}'")
+        return self._inner_matcher(doc)
 
     def _parse_match(self, match, doc):
         logger.debug(f"Parsing match '{match}'")
--- src/detectors.py
+++ src/detectors.py
@@ -3,6 +3,7 @@ import logging
 import os
 from json import load
 from settings import PATTERNS_DIR
+from .extractors import get_doc
 from .Matcher import Matcher
 from .patterns import load_pattern_set
 
@@ -19,11 +20,27 @@ async def detect_features(sentence, pattern_set_names):
 
     feature_set = {}
     matcher = Matcher()
+
+    logger.debug(f"Tokenizing '{sentence}'")
+    doc = get_doc(sentence)
+
     for pattern_set_name in pattern_set_names:
         logger.debug(f"Detecting for the feature '{pattern_set_name}'")
         logger.debug(f"Loading the pattern set '{pattern_set_name}'")
         pattern_set = load_pattern_set(pattern_set_name)
 
+        # Noun chunks
+        logger.debug("Determining if the noun chunks should be extracted")
+        should_extract_noun_chunks = False
+        try:
+            should_extract_noun_chunks = pattern_set["meta"]["extract_noun_chunks"]
+        except KeyError:
+            should_extract_noun_chunks = False
+
+        logger.debug(f"Extracting the noun chunks: {should_extract_noun_chunks}")
+        inputs = doc.noun_chunks if should_extract_noun_chunks else [doc]
+
+        # Deciding the appropriate matcher function
         logger.debug(f"Determining how many matches should be returned")
         how_many_matches = None
         try:
@@ -34,21 +51,24 @@ async def detect_features(sentence, pattern_set_names):
         logger.debug(
             f"Determining the detector function for '{how_many_matches}' result(s)."
         )
-        result = None
-        if how_many_matches == "ONE":
-            logger.debug(f"Matching one")
-            result = matcher.match_one(pattern_set, sentence)
-        elif how_many_matches == "MANY":
-            logger.debug(f"Matching many")
-            result = matcher.match_many(pattern_set, sentence)
-        else:  # Search for one or many by default
-            logger.debug(f"Matching with default '{DEFAULT_HOW_MANY_MATCHES}'")
-            result = matcher.match_one(pattern_set, sentence)
+        results = []
+        for input in inputs:
+            result = None
+            if how_many_matches == "ONE":
+                logger.debug(f"Matching one")
+                result = matcher.match_one(pattern_set, input)
+            elif how_many_matches == "MANY":
+                logger.debug(f"Matching many")
+                result = matcher.match_many(pattern_set, input)
+            else:  # Search for one or many by default
+                logger.debug(f"Matching with default '{DEFAULT_HOW_MANY_MATCHES}'")
+                result = matcher.match_one(pattern_set, input)
+            results.append(result)
 
         logger.debug(
-            f"Setting the result to the feature set's '{pattern_set_name}' key"
+            f"Setting the results to the feature set's '{pattern_set_name}' key"
         )
-        feature_set[pattern_set_name] = result
+        feature_set[pattern_set_name] = results
 
         logger.debug(f"Finished detecting for the feature '{pattern_set_name}'")
 
