058a7de578e8b3a112188c91965c416f9a61eea0
SKCrawford <s.kyle.crawford@gmail.com>
2021-10-05 11:48:32 -0700

    Use subtests when unittesting the json's test list.

--- tests/test_patternsets_json.py
+++ tests/test_patternsets_json.py
@@ -26,22 +26,26 @@ class TestPatternSetJsonTests(unittest.TestCase):
             matcher = self.matchers[pset_key]
 
             if pset.tests:
-                for test in pset.tests:
-                    input = get_doc(test["input"])
-                    expected_rulename = test["rulename"] if "rulename" in test else None
-                    expected_span = test["span"] if "span" in test else None
+                with self.subTest(pset.name):
+                    for t in pset.tests:
+                        input = get_doc(t["input"])
+                        expected_rulename = t["rulename"] if "rulename" in t else None
+                        expected_span = t["span"] if "span" in t else None
 
-                    if not expected_rulename and not expected_span:
-                        err_msg = "The test entry must have at least one of these keys: expected_rulename and/or expected_span"
-                        raise KeyError(err_msg)
+                        if not expected_rulename and not expected_span:
+                            err_msg = "The test entry must have at least one of these keys: expected_rulename and/or expected_span"
+                            raise KeyError(err_msg)
 
-                    (rulename, _, features) = matcher.match(input)
-                    span = str(features["span"])
+                        (rulename, span1, features) = matcher.match(input)
+                        span1 = str(span1)
+                        span2 = str(features["span"])
 
-                    if expected_rulename:
-                        err_msg = f"'{input}': expected rulename '{expected_rulename}' but got '{rulename}'"
-                        self.assertEqual(rulename, expected_rulename, err_msg)
+                        if expected_rulename:
+                            err_msg = f"'{input}': expected rulename '{expected_rulename}' but got '{rulename}'"
+                            self.assertEqual(rulename, expected_rulename, err_msg)
 
-                    if expected_span:
-                        err_msg = f"'{input}': expected span '{expected_span}' but got '{span}'"
-                        self.assertEqual(span, expected_span, err_msg)
+                        if expected_span:
+                            err_msg = f"'{input}': expected span '{expected_span}' but got '{span1}'"
+                            self.assertEqual(span1, expected_span, err_msg)
+                            err_msg = f"'{input}': expected span '{expected_span}' but got '{span2}'"
+                            self.assertEqual(span2, expected_span, err_msg)
