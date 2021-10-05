77d8ab5966a4cf4f1d1377aabec8970a91663bb2
SKCrawford <s.kyle.crawford@gmail.com>
2021-10-05 13:44:48 -0700

    Bugfix: the setting PATTERN_SET_PATHS now correctly ignores hidden files.

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 PATTERNS_DIR = os.path.join(ROOT_DIR, "patterns/")
 
-PATTERN_SETS_PATHS = os.listdir(PATTERNS_DIR)
+PATTERN_SETS_PATHS = [f for f in os.listdir(PATTERNS_DIR) if not f.startswith(".")]
 
 PATTERN_SETS_NAMES = [psp.replace(".json", "") for psp in PATTERN_SETS_PATHS]
