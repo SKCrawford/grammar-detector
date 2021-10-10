df7fe5171949d174a7f84e05c851b6c8f0550e65
SKCrawford <s.kyle.crawford@gmail.com>
2021-10-09 19:39:45 -0700

    Remove the old tests that were retained for reference.

--- tests-old/core/matcher/test_parse_parse_match.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from src.core.matcher.parse import parse_match
-
-
-class TestCoreMatcherParseMatch(unittest.TestCase):
-    def test_is_defined(self):
-        self.assertTrue(parse_match)
--- tests-old/core/test_transformer.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from src.core import extract_span_features
-
-
-class TestPhraseFeatureParser(unittest.TestCase):
-    def test_is_defined(self):
-        self.assertTrue(extract_span_features)
