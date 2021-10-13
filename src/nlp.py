c79e41d3915f10371ab9fbdc409b4aeef86368d4
SKCrawford <s.kyle.crawford@gmail.com>
2021-10-12 17:40:18 -0700

    Add a note about downloading the spacy dataset. This would best be addressed in the eventual readme.

--- src/nlp.py
+++ src/nlp.py
@@ -6,5 +6,7 @@ from settings import SPACY_DATASET
 logging.getLogger(__name__).info(
     f"Started loading the spaCy model with the dataset '{SPACY_DATASET}'"
 )
+# Don't forget to run (with the appropriate dataset):
+# $ python -m download en_core_web_lg
 nlp = spacy.load(SPACY_DATASET)
 logging.getLogger(__name__).info("Finished loading the spaCy model")
