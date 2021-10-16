cf5988d482205aeed184b2ea7f808e3b210e9fbf
SKCrawford <s.kyle.crawford@gmail.com>
2021-10-15 19:06:44 -0700

    Refactor out business logic from the detect_features method to the new detect_feature method (note singularity).

--- src/detectors.py
+++ src/detectors.py
@@ -5,11 +5,36 @@ from spacy.tokens import Doc, Span
 from typing import Any, Union
 from .extractors import get_doc
 from .loaders import PatternSetLoader, YamlLoader
-from .matchers import ParsedMatch, PatternSetMatcher as Matcher
+from .matchers import ParsedMatch, PatternSetMatcher
 from .patterns import PatternSet
 
 
 logger = getLogger(__name__)
+
+
+async def detect_feature(doc: Doc, pattern_set_name: str) -> dict[str, Any]:
+    logger.debug(f"Started detecting for the feature '{pattern_set_name}'")
+    logger.debug(f"Loading the patternset '{pattern_set_name}'")
+    pattern_set: PatternSet = pset_loader(pattern_set_name)
+
+    logger.debug("Constructing the PatternSetMatcher")
+    matcher = PatternSetMatcher(pattern_set)
+
+    logger.debug("Determining if the noun chunks should be extracted from the doc")
+    key: str = SettingKeys.PSET_META_EXTRACT_NOUN_CHUNKS.value
+    should_extract_noun_chunks: bool = bool(pattern_set.meta[key])
+
+    inputs: list[Union[Doc, Span]] = []
+    if should_extract_noun_chunks:
+        logger.debug("Extracting the noun chunks from the doc")
+        inputs = doc.noun_chunks
+    else:
+        inputs = [doc]
+
+    logger.debug("Running the PatternSetMatcher")
+    matches: list[list[ParsedMatch]] = [matcher(input) for input in inputs]
+    logger.debug(f"Finished detecting for the feature '{pattern_set_name}'")
+    return matches
 
 
 async def detect_features(
@@ -27,30 +52,8 @@ async def detect_features(
     logger.debug(f"Tokenizing '{sentence}'")
     doc: Doc = get_doc(sentence)
 
-    for pattern_set_name in pattern_set_names:
-        logger.debug(f"Detecting for the feature '{pattern_set_name}'")
-        logger.debug(f"Loading the pattern set '{pattern_set_name}'")
-        pattern_set: PatternSet = pset_loader(pattern_set_name)
-
-        logger.debug("Constructing the external matcher")
-        matcher = Matcher(pattern_set)
-
-        # Noun chunks
-        logger.debug("Determining if the noun chunks should be extracted from the doc")
-        key: str = SettingKeys.PSET_META_EXTRACT_NOUN_CHUNKS.value
-        should_extract_noun_chunks: bool = bool(pattern_set.meta[key])
-
-        inputs: list[Union[Doc, Span]] = []
-        if should_extract_noun_chunks:
-            logger.debug("Extracting the noun chunks")
-            inputs = doc.noun_chunks
-        else:
-            inputs = [doc]
-
-        logger.debug(f"Running the external matcher")
-        matches: list[list[ParsedMatch]] = [matcher(input) for input in inputs]
-        feature_set[pattern_set_name] = matches
-        logger.debug(f"Finished detecting for the feature '{pattern_set_name}'")
+    for pset_name in pattern_set_names:
+        feature_set[pset_name] = await detect_feature(doc, pset_name)
 
     logger.debug(f"Finished detecting for the features '{pattern_set_names}'")
     return feature_set
