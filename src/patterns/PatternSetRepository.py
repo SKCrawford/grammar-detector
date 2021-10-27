4ec72d984b82182fc9b0802f1f6eae3bde8996ca
SKCrawford <s.kyle.crawford@gmail.com>
2021-10-26 23:35:46 -0700

    Refactor out the Repository.

--- src/patterns/PatternSetRepository.py
+++ src/patterns/PatternSetRepository.py
@@ -1,6 +1,7 @@
 from logging import getLogger
 from typing import cast
 from ..cache import Cacheable
+from ..repositories import Repository
 from .Pattern import extract_pattern_data, Pattern, PatternData
 from .PatternSet import extract_pattern_set_data, PatternSet, PatternSetData
 from .PatternSet import Name
@@ -9,7 +10,32 @@ from .PatternSet import Name
 logger = getLogger(__name__)
 
 
-class PatternSetRepository(Cacheable):
+class PatternSetRepository(Repository):
+    def __init__(self):
+        super().__init__(PatternSet, cache_key="name")
+
+    def create(self, name: Name, data: PatternSetData) -> PatternSet:
+        """Create a new PatternSet instance, cache it, and return it. If a PatternSet with the same name has already been created, the cached instance will be returned."""
+        if self.cache.has_key(name):
+            return self.get_one(name)
+
+        (pattern_data_list, meta, tests) = extract_pattern_set_data(data)
+        pattern_set = super().create(name)
+        pattern_set.meta = meta
+        pattern_set.tests = tests
+        for pattern_data in pattern_data_list:
+            self._register_pattern(pattern_data, pattern_set)
+
+        self.cache.save(pattern_set.name, pattern_set)
+        return pattern_set
+
+    def _register_pattern(self, data: PatternData, pattern_set: PatternSet) -> None:
+        """Extract the components of the raw pattern data, create a Pattern, and add it to the PatternSet."""
+        (rulename, tokens) = extract_pattern_data(data)
+        pattern_set.add_pattern(Pattern(rulename, tokens))
+
+
+class PatternSetRepository2(Cacheable):
     """The class for creating, caching, and retrieving PatternSet instances."""
 
     def create(self, name: Name, data: PatternSetData) -> PatternSet:
--- src/repositories.py
+++ src/repositories.py
@@ -6,18 +6,27 @@ T = TypeVar("T")
 
 
 class Repository(Generic[T]):
-    def __init__(self, klass: T) -> None:
-        self._klass = klass
+    def __init__(self, klass: T, cache_key: str = "id") -> None:
+        """The `cache_key` option changes the key from the `klass` instance's `int id` as a `str` to some other `str`. This changes the attribute by which the repository saves and searches. A `cache_key` of `id` caches the `klass` instances by their `id()` as a `str`. A `cache_key` of any other `str` will cache by that attribute of the `klass` instance."""
+        self._klass: T = klass
         self._cache = Cache()
+        self._cache_key: str = cache_key
 
-    def create(self, *args, **kwargs) -> T:
-        instance = self._klass(*args, **kwargs)
-        self._cache.save(id(instance), instance)
-        return instance
+    def create(self, *args: str, **kwargs: str) -> T:
+        instance: T = self._klass(*args, **kwargs)
+        cache_key = self._make_key(instance)
+        if self._cache.has_key(cache_key):
+            return self.get_one(cache_key)
+        self._cache.save(cache_key, instance)
+        return self.get_one(cache_key)
 
     def get_all(self) -> list[T]:
         return self._cache.get_all()
 
-    def get_one(self, id: int) -> T:
-        id = int(id)
-        return self._cache.get_one(id)
+    def get_one(self, key: str) -> T:
+        return self._cache.get_one(str(key))
+
+    def _make_key(instance: T) -> str:
+        if self._cache_key != "id":
+            return str(instance[self._cache_key])
+        return str(id(instance))
