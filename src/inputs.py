1d25660425198e86b9cf071c78999a1f4e461a4a
SKCrawford <s.kyle.crawford@gmail.com>
2021-10-29 19:03:05 -0700

    Improve docstrings for inputs.

--- src/inputs.py
+++ src/inputs.py
@@ -4,7 +4,7 @@ from .nlp import nlp
 
 
 class Input:
-    """A helper class for tokenizing text and fragmenting the result into a list of Docs. This is required because of the support for extracting noun chunks, which is always a `list[Span]`. As such, a single `Doc` is wrapped in a `list`, hence the `docs` property."""
+    """A helper class for tokenizing text and fragmenting the result into a `list[Doc]`. All properties must return a `list[Doc]` for compatibility purposes, hence the `docs` property."""
 
     def __init__(self, raw_input: str, extract_noun_chunks: bool = False) -> None:
         self.raw: str = raw_input
@@ -13,7 +13,7 @@ class Input:
 
     @property
     def docs(self) -> list[Doc]:
-        """Wrap the core Doc in a list for compatibility with the noun chunk extraction feature."""
+        """Wrap the core Doc in a list for compatibility purposes."""
         return [self.doc]
 
     @property
