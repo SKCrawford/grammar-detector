c3f12b778d9bac10edeba1aeb5392069286198ff
SKCrawford <s.kyle.crawford@gmail.com>
2021-10-29 18:40:44 -0700

    Improve logic and type safety for the get_doc method

--- src/extractors.py
+++ src/extractors.py
@@ -1,7 +1,7 @@
 from logging import getLogger
 from spacy import explain
 from spacy.tokens import Doc, Span
-from typing import Any, cast, Union
+from typing import Any, Union
 from .nlp import nlp
 
 
@@ -11,28 +11,27 @@ SpanFeatures = dict[str, str]
 logger = getLogger(__name__)
 
 
-def get_doc(phrase: Union[str, Doc, Span]) -> Doc:
-    logger.debug(f"Getting the Doc of '{phrase}' ({type(phrase)})")
-    doc: Doc = None
-    phrase_type = type(phrase)
-    if phrase_type == str:
-        phrase = cast(str, phrase)
-        doc = nlp(phrase)
-    elif phrase_type == Doc:
-        phrase = cast(Doc, phrase)
-        doc = phrase
-    elif phrase_type == Span:
-        phrase = cast(Span, phrase)
-        doc = phrase
+def get_doc(text: Union[str, Doc, Span]) -> Doc:
+    text_type = type(text)
+
+    logger.debug(f"Getting the Doc for '{text}' (text_type)")
+    if isinstance(text_type, Doc):
+        logger.debug("Skipping tokenizing")
+        return text
+    elif isinstance(text_type, Span):
+        logger.debug("Converting the Span to a Doc")
+        return text.as_doc()
+    elif isinstance(text_type, str):
+        logger.debug("Tokenizing the raw str")
+        return nlp(text)
     else:
-        msg = f"Cannot get the Doc of '{phrase}' ({phrase_type})"
+        msg = f"Cannot get the Doc for '{text}' ({text_type})"
         logger.error(msg)
         raise TypeError(msg)
-    return doc
 
 
 def extract_span_features(match_span: Span) -> SpanFeatures:
-    logger.debug(f"Parsing Span '{match_span}'")
+    logger.debug(f"Parsing the '{match_span}' Span")
     return {
         "span": match_span,
         "phrase": match_span.text,
--- src/inputs.py
+++ src/inputs.py
@@ -1,11 +1,10 @@
 from spacy.tokens import Doc, Span
 from .extractors import get_doc
 from .nlp import nlp
-from .utils import Doclike
 
 
 class Input:
-    """A helper class for tokenizing text and fragmenting the result into a list of Doclikes. This is required because of the support for extracting noun chunks, which is always a `list[Span]`. As such, a single `Doc` is wrapped in a `list`, hence the `docs` property."""
+    """A helper class for tokenizing text and fragmenting the result into a list of Docs. This is required because of the support for extracting noun chunks, which is always a `list[Span]`. As such, a single `Doc` is wrapped in a `list`, hence the `docs` property."""
 
     def __init__(self, raw_input: str, extract_noun_chunks: bool = False) -> None:
         self.raw: str = raw_input
@@ -18,12 +17,12 @@ class Input:
         return [self.doc]
 
     @property
-    def noun_chunks(self) -> list[Span]:
+    def noun_chunks(self) -> list[Doc]:
         """Get all of the nouns as Docs from the core Doc."""
-        return self.doc.noun_chunks
+        return [span.as_doc() for span in self.doc.noun_chunks]
 
     @property
-    def fragments(self) -> list[Doclike]:
+    def fragments(self) -> list[Doc]:
         """Automatically select the most appropriate fragmenting attribute."""
         if self.extract_noun_chunks:
             return self.noun_chunks
