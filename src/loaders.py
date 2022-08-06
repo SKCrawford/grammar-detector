a8f654e2bf42333d6afe456d81bc0ebcf9c8168b
SKCrawford <s.kyle.crawford@gmail.com>
2022-08-05 20:54:14 -0700

    Refactor the FileLoader and YamlLoader to accept a full filepath instead of taking a filename and generating a filepath.

--- src/loaders.py
+++ src/loaders.py
@@ -1,5 +1,4 @@
 from logging import getLogger
-from os.path import join
 from typing import Any, TextIO
 from yaml import FullLoader, load as load_yaml
 from .utils import singleton
@@ -10,27 +9,9 @@ logger = getLogger(__name__)
 
 # @singleton  # the parent inherits the child's singleton decorator
 class FileLoader:
-    def __init__(self, file_ext: str, dir_path: str) -> None:
-        logger.info(f"Constructing the FileLoader")
-        if not file_ext:
-            msg = f"argument 'file_ext' is missing"
-            logger.error(msg)
-            raise ValueError(msg)
-        if not dir_path:
-            msg = f"argument 'dir_path' is missing"
-            logger.error(msg)
-            raise ValueError(msg)
-
-        self.file_ext: str = file_ext
-        self.dir_path: str = dir_path
-
-    def __call__(self, filename_base: str) -> dict[str, Any]:
-        path: str = self.generate_path(filename_base)
-        with open(path, "r") as file:
+    def __call__(self, filepath: str) -> dict[str, Any]:
+        with open(filepath, "r") as file:
             return self.load(file)
-
-    def generate_path(self, filename_base: str) -> str:
-        return join(self.dir_path, f"{filename_base}.{self.file_ext}")
 
     def load(self, file: TextIO) -> dict[str, Any]:
         msg = f"load(file) was not implemented"
@@ -40,10 +21,6 @@ class FileLoader:
 
 @singleton
 class YamlLoader(FileLoader):
-    def __init__(self, *args: str, **kwargs: str) -> None:
-        logger.info(f"Constructing the YamlLoader")
-        super().__init__("yaml", *args, **kwargs)
-
     def load(self, file: TextIO) -> Any:
         logger.debug(f"Loading the YAML file: {file}")
         return load_yaml(file, Loader=FullLoader)
