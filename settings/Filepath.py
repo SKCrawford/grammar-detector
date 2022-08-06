50c084b01d122dac1c0a6a81f3256c7b28180ce8
SKCrawford <s.kyle.crawford@gmail.com>
2022-08-05 21:06:49 -0700

    Bugfix: attributes now work correctly.

--- settings/Filepath.py
+++ settings/Filepath.py
@@ -10,15 +10,17 @@ class Filepath:
     """A helper class for working with filepaths, especially with extracting a filepath's components."""
 
     def __init__(self, filepath: str) -> None:
-        self._filepath: str = ""
+        self._filepath: str = str(filepath)
         self.dir_path: str = ""
         self.filename: str = ""
         self.name: str = ""
         self.extension: str = ""
 
-        self._filepath = str(filepath)
-        [self.dir_path, self.filename] = split(self._filepath)
-        [self.name, self.extension] = self.filename.split(".")
+        (self.dir_path, self.filename) = split(self._filepath)
+        fname_splits = self.filename.split(".")
+        # Use -2 instead of 0 because hidden files's 0 is an empty string
+        self.name = fname_splits[-2]
+        self.extension = fname_splits[-1]
 
     @property
     def filepath(self) -> str:
